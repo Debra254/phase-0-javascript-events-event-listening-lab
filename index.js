c6cd3e9e07ce65efd49aab1d15e132f8013c1177
Debra <debrawangeci@gmail.com>
2025-06-22 17:16:33 +0300

    add changes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function addingEventListener() {
-  const input = document.getElementById('input');
-  input.addEventListener('click', function () {
+  const input = document.getElementById('button');
+
+  function clickAlert() {
     alert('I was clicked!');
-  });
+  }
+
+  input.addEventListener('click', clickAlert);
 }
+
+// Call the function to activate the event listener
+addingEventListener();
+
 
 
 
